f8f3a0b438448187bcc59927898ae393f3cd5057
pastelnata <ritabb04.05@gmail.com>
2024-03-12 20:50:35 +0100

    Add ProductionUnit class

--- HeatProductionOptimizer.Tests/UnitTest1.cs
+++ HeatProductionOptimizer.Tests/UnitTest1.cs
@@ -1,10 +1,27 @@
 namespace HeatProductionOptimizer.Tests;
 
-public class UnitTest1
+public class AssetManagerTests
 {
     [Fact]
-    public void Test1()
+    public void ProductionUnit_WithValidParameters_SetsPropertiesCorrectly()
     {
+        //ARRANGE
+        string name = "GB";
+        double maxHeat = 5.0;
+        int productionCosts = 500;
+        int co2Emissions = 215;
+        double gasConsumption = 1.1;
+        double maxElectricity = 2.7;
 
+        //ACT
+        ProductionUnit gasBoiler = new ProductionUnit(name, maxHeat, productionCosts, co2Emissions, gasConsumption, maxElectricity);
+
+        //ASSERT
+        Assert.Equal(name, gasBoiler.GetName());
+        Assert.Equal(maxHeat, gasBoiler.GetMaxHeat());
+        Assert.Equal(productionCosts, gasBoiler.GetProductionCosts());
+        Assert.Equal(co2Emissions, gasBoiler.GetCO2Emissions());
+        Assert.Equal(gasConsumption, gasBoiler.GetGasConsumption());
+        Assert.Equal(maxElectricity, gasBoiler.GetMaxElectricity());
     }
 }
---
+++ HeatProductionOptimizer/AssetManager.cs
@@ -0,0 +1,55 @@
+public class AssetManager
+{
+    static ProductionUnit gasBoiler = new ProductionUnit("GB", 5.0, 500, 215, 1.1, 0);
+    static ProductionUnit oilBoiler = new ProductionUnit("OB", 4.0, 700, 265, 1.2, 0);
+    static ProductionUnit gasMotor = new ProductionUnit("GM", 3.6, 1100, 640, 1.9, 2.7);
+    static ProductionUnit electricBoiler = new ProductionUnit("EK", 8.0, 50, 0, 0, -8.0);
+}
+
+public class ProductionUnit
+{
+    private string Name;
+    private double MaxHeat;
+    private int ProductionCosts;
+    private int CO2Emissions;
+    private double GasConsumption;
+    private double MaxElectricity;
+    //private string GraphicalRepresentationFilePath; // path to the image
+
+    //todo add image
+    public ProductionUnit(string name, double maxHeat, int productionCosts, int co2Emissions, double gasConsumption, double maxElectricity)
+    {
+        Name = name;
+        MaxHeat = maxHeat;
+        ProductionCosts = productionCosts;
+        CO2Emissions = co2Emissions;
+        GasConsumption = gasConsumption;
+        MaxElectricity = maxElectricity;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+    public double GetMaxHeat()
+    {
+        return MaxHeat;
+    }
+    public int GetProductionCosts()
+    {
+        return ProductionCosts;
+    }
+    public int GetCO2Emissions()
+    {
+        return CO2Emissions;
+    }
+    public double GetGasConsumption()
+    {
+        return GasConsumption;
+    }
+    public double GetMaxElectricity()
+    {
+        return MaxElectricity;
+    }
+
+}
