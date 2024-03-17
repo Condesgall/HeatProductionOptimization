2d02fd529745207ddafa44675dd582575208f5a1
sduhubert <husyl23@student.sdu.dk>
2024-03-17 19:56:54 +0100

    presented functionality of SDM functions

--- HeatProductionOptimizer/Program.cs
+++ HeatProductionOptimizer/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Loading is now fully functional, down below is a quick presentation:
+// 1. Declare the file path, and two lists for 2 heating seasons (winter, summer)
+// 2. Create a new ParameterLoader instance, input the file path as a parameter, Load the data
+// 3. Data is accessible through <instancename>.Winter and <instancename>.Summer, corresponding to heating seasons
+// 4. Ive shown an example on how to display all of the data.
+// NOTE: TimeFrom and TimeTo are still in string format, for now i didn't see the reason to convert them
+
+// If you want to contribute with some more work, you can try and make commands to visualize all of the data
+// in a nicer way, and outside of Program.cs.
+
+string filePath = "SourceData.csv";
+
+ParameterLoader SourceData = new ParameterLoader(filePath);
+SourceData.Load();
+
+Console.Clear();
+
+Console.WriteLine("Winter data:");
+Console.WriteLine();
+foreach(SdmParameters param in SourceData.Winter)
+{
+    Console.WriteLine($"Time from: {param.TimeFrom}");
+    Console.WriteLine($"Time to: {param.TimeTo}");
+    Console.WriteLine($"Heat demand: {param.HeatDemand}");
+    Console.WriteLine($"Electricity price: {param.ElPrice}");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Summer data:");
+Console.WriteLine();
+foreach(SdmParameters param in SourceData.Summer)
+{
+    Console.WriteLine($"Time from:{param.TimeFrom}");
+    Console.WriteLine($"Time to:{param.TimeTo}");
+    Console.WriteLine($"Heat demand:{param.HeatDemand}");
+    Console.WriteLine($"Electricity price:{param.ElPrice}");
+    Console.WriteLine();
+}
