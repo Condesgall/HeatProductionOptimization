c3c30b1ecff6314cfee5cb461ceb35d1087dbade
sduhubert <husyl23@student.sdu.dk>
2024-03-17 19:56:27 +0100

    Add SdmParameters and ParameterLoader

--- HeatProductionOptimizer/SourceDataManager.cs
+++ HeatProductionOptimizer/SourceDataManager.cs
@@ -1,42 +1,74 @@
 using System;
+using System.Globalization;
 
 public class SdmParameters
 {
     public string TimeFrom {get; set;}
     public string TimeTo {get; set;}
-    public string HeatDemand {get; set;}
-    public string Price {get; set;}
+    public double HeatDemand {get; set;}
+    public double ElPrice {get; set;}
 
-
-    public void Load(string filePath, List<SdmParameters> summerList, List<SdmParameters> winterList)
+    public SdmParameters(string timeFrom, string timeTo, double heatDemand, double elPrice)
     {
-        using (var reader = new StreamReader(filePath))
-        {
-            // Skip the header line
-            reader.ReadLine();
+        TimeFrom = timeFrom;
+        TimeTo = timeTo;
+        HeatDemand = heatDemand;
+        ElPrice = elPrice;
+    }
+}
 
-            // Process each line in the CSV file
-            string line;
-            while ((line = reader.ReadLine()) != null)
-            {
-                // Split the line by comma
-                string[] parts = line.Split(',');
+public class ParameterLoader
+{
+    public List<SdmParameters> Winter { get; private set; }
+    public List<SdmParameters> Summer { get; private set; }
+    
+    private string FilePath;
 
-                foreach(string part in parts)
-                {
-                    System.Console.Write($"{part}\t");
-                }
-                System.Console.WriteLine();
-            }
-        }
+    public ParameterLoader(string filePath)
+    {
+        Winter = new List<SdmParameters>();
+        Summer = new List<SdmParameters>();
+        FilePath = filePath;
     }
 
-
-    public SdmParameters(string timeFrom, string timeTo, double heatDemand, double price)
+    public void Load()
     {
-        timeFrom = TimeFrom;
-        timeTo = TimeTo;
-        heatDemand = HeatDemand;
-        price = Price;    
+        int i = 0;
+        using (var reader = new StreamReader(FilePath))
+        {
+            // Going line by line, reading all the parameters from each.
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] lineParts = line.Split(',');
+                
+                // Skipping the start of the file and other possibly unnecessary fields.
+                if(!lineParts[0].Contains("/")) 
+                {
+                    continue;
+                }
+                else
+                {
+                    // Inputting the read data into temporary objects (the InvariantCulture part is to make sure
+                    // it takes "." as a decimal point, and not a separator as in some languages.
+                    SdmParameters currentWinterParameters = new(
+                        lineParts[0], 
+                        lineParts[1], 
+                        double.Parse(lineParts[2], CultureInfo.InvariantCulture),
+                        double.Parse(lineParts[3], CultureInfo.InvariantCulture));
+
+                    SdmParameters currentSummerParameters = new(
+                        lineParts[5],
+                        lineParts[6],
+                        double.Parse(lineParts[7], CultureInfo.InvariantCulture),
+                        double.Parse(lineParts[8], CultureInfo.InvariantCulture));
+
+                    Winter.Add(currentWinterParameters);
+                    Summer.Add(currentSummerParameters);
+
+                    i++;
+                }
+            }
+        }  
     }
 }
