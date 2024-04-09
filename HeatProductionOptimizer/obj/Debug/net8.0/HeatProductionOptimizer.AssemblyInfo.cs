a15f8e8deb8fd360f670bc68fb71a8a720a0c46e
Dobos23 <domimisb@gmail.com>
2024-04-09 21:24:21 +0200

    Made SourceDataManager work

--- HeatProductionOptimizer/obj/Debug/net8.0/HeatProductionOptimizer.AssemblyInfo.cs
+++ HeatProductionOptimizer/obj/Debug/net8.0/HeatProductionOptimizer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatProductionOptimizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a805ef0ad8b9f6fc95055effd0f28a56b1e388")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789b715bdd42766753cfa6a9dee8e28a2941d3d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatProductionOptimizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatProductionOptimizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
