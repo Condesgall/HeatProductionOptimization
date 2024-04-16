54e5f7569ce21686486d48eab8167880da7489f4
Jakub Smilowski <jakub.smilowski@protonmail.com>
2024-04-16 15:15:33 +0200

    Added part of the optimizer

--- HeatProductionOptimizer/Optimizer.cs
+++ HeatProductionOptimizer/Optimizer.cs
@@ -44,4 +44,77 @@ public class Optimizer
             }
         }
     }
+
+     public void OptimizeProduction(ParameterLoader parameterLoader, AssetManager_.AssetManager assetManager, ResultDataManager_.ResultDataManager resultDataManager)
+    {
+        double producedHeat = 0;
+        double producedElectricity = 0;
+        double consumedElectricity = 0;
+        double expenses = 0;
+        double profit = 0;
+        double primaryEnergyConsumption = 0;
+        double co2Emissions = 0;
+
+        // Loop  winter data
+        foreach (var winterData in parameterLoader.Winter)
+        {
+            foreach (var productionUnit in AssetManager_.AssetManager.productionUnits)
+            {
+                //Only for scenario 1
+                if (productionUnit.Name == "GB" || productionUnit.Name == "OB" ){
+                    
+                    
+                    
+                    
+                    var optimizationResults = new ResultDataManager_.OptimizationResults(
+                        producedHeat,
+                        producedElectricity,
+                        consumedElectricity,
+                        expenses,
+                        profit,
+                        primaryEnergyConsumption,
+                        co2Emissions
+                    );
+
+                    // Add optimization results
+                    resultDataManager.AddResultData(productionUnit.Name, optimizationResults);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        // Loop summer data
+        foreach (var summerData in parameterLoader.Summer)
+        {
+            foreach (var productionUnit in AssetManager_.AssetManager.productionUnits)
+            {
+               //Only for scenario 1
+                if (productionUnit.Name == "GB" || productionUnit.Name == "OB" ){
+                    
+                    
+                    
+                    
+                    var optimizationResults = new ResultDataManager_.OptimizationResults(
+                        producedHeat,
+                        producedElectricity,
+                        consumedElectricity,
+                        expenses,
+                        profit,
+                        primaryEnergyConsumption,
+                        co2Emissions
+                    );
+
+                    // Add optimization results
+                    resultDataManager.AddResultData(productionUnit.Name, optimizationResults);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
 }
