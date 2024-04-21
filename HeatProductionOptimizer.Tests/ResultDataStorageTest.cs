8d24b1b8f2391f14d7baa7962557c7f7f5d0b985
sduhubert <husyl23@student.sdu.dk>
2024-04-21 19:44:07 +0200

    first commit

--- HeatProductionOptimizer.Tests/ResultDataStorageTest.cs
+++ HeatProductionOptimizer.Tests/ResultDataStorageTest.cs
@@ -21,31 +21,32 @@ namespace ResultDataStorage.Tests
 
             // Assert
             Assert.NotNull(resultDataCSV.loadedResultData);
-            Assert.NotEmpty(resultDataCSV.loadedResultData.resultData);
-            Assert.Contains("GB", resultDataCSV.loadedResultData.resultData.Keys);
+            Assert.NotEmpty(resultDataCSV.loadedResultData);
+            Assert.Contains("GB", resultDataCSV.loadedResultData[0].ProductionUnit);
 
             // Verify the values of loaded OptimizationResults
-            OptimizationResults gbResults = resultDataCSV.loadedResultData.resultData["GB"];
-            Assert.Equal(1.0, gbResults.ProducedHeat);
-            Assert.Equal(2.0, gbResults.ProducedElectricity);
-            Assert.Equal(3.0, gbResults.ConsumedElectricity);
-            Assert.Equal(4.0, gbResults.Expenses);
-            Assert.Equal(5.0, gbResults.Profit);
-            Assert.Equal(6.0, gbResults.PrimaryEnergyConsumption);
-            Assert.Equal(7.0, gbResults.Co2Emissions);
+            OptimizationResults gbResults = resultDataCSV.loadedResultData[0].OptimizationResults;
+            Assert.Equal(1, gbResults.ProducedHeat);
+            Assert.Equal(2, gbResults.ProducedElectricity);
+            Assert.Equal(3, gbResults.ConsumedElectricity);
+            Assert.Equal(4, gbResults.Expenses);
+            Assert.Equal(5, gbResults.Profit);
+            Assert.Equal(6, gbResults.PrimaryEnergyConsumption);
+            Assert.Equal(7, gbResults.Co2Emissions);
         }
 
         [Fact]
         public void Save_CorrectlyWritesDataToFile()
         {
-            string testFilePath = Path.Combine(assemblyDirectory, "Test.csv"); 
-            var resultDataManager = new ResultDataManager();
-            OptimizationResults testData = new OptimizationResults(1, 2, 3, 4, 5, 6, 7);
-            resultDataManager.AddResultData("GB", testData);
+            string testFilePath = Path.Combine(assemblyDirectory, "TestRDM.csv"); 
+            OptimizationResults testData = new OptimizationResults(1.0m, 2.0m, 3.0m, 4.0m, 5.0m, 6.0m, 7.0m);
             ResultDataCSV resultDataCSV = new ResultDataCSV(testFilePath);
 
-            resultDataCSV.loadedResultData = resultDataManager; 
-            resultDataCSV.Save();
+            ResultData testResultData = new ResultData("1/1/1970 0:00", "1/1/1970 1:00", "GB", testData);
+
+            ResultDataManager.Winter.Add(testResultData);
+
+            resultDataCSV.Save(ResultDataManager.Winter);
 
             // Assert
             // Read the saved file and verify its contents match the expected data
@@ -57,14 +58,16 @@ namespace ResultDataStorage.Tests
                 string line = reader.ReadLine();
                 string[] lineParts = line.Split(',');
 
-                Assert.Equal("GB", lineParts[0]);
-                Assert.Equal("1", lineParts[1]);
-                Assert.Equal("2", lineParts[2]);
-                Assert.Equal("3", lineParts[3]);
-                Assert.Equal("4", lineParts[4]);
-                Assert.Equal("5", lineParts[5]);
-                Assert.Equal("6", lineParts[6]);
-                Assert.Equal("7", lineParts[7]);
+                Assert.Equal("1/1/1970 0:00", lineParts[0]);
+                Assert.Equal("1/1/1970 1:00", lineParts[1]);
+                Assert.Equal("GB", lineParts[2]);
+                Assert.Equal("1.0", lineParts[3]);
+                Assert.Equal("2.0", lineParts[4]);
+                Assert.Equal("3.0", lineParts[5]);
+                Assert.Equal("4.0", lineParts[6]);
+                Assert.Equal("5.0", lineParts[7]);
+                Assert.Equal("6.0", lineParts[8]);
+                Assert.Equal("7.0", lineParts[9]);
             }
         }
     }
