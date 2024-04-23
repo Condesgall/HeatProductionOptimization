7edb4d705acf741543bff00d1596470bbd8e2ecb
Condegall <sandrag@outlook.dk>
2024-04-23 14:49:49 +0200

    ChangeMainContentMessage

--- HeatProductionOptimizer/HeatingGridAvaloniApp/obj/Debug/net8.0/HeatingGridAvaloniApp.AssemblyInfo.cs
+++ HeatProductionOptimizer/HeatingGridAvaloniApp/obj/Debug/net8.0/HeatingGridAvaloniApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatingGridAvaloniApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1591cce2f1378c11610097f063e902c1cc124bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66f74a97c2381d1102cd7c811e80d042dd4836d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatingGridAvaloniApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatingGridAvaloniApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
