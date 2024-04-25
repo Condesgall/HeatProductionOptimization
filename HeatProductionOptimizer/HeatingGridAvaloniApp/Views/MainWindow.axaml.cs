968249b53d801ecabffe8b1f4d5aeec4b76dbc17
Condegall <sandrag@outlook.dk>
2024-04-25 17:39:03 +0200

    DecideAccessAppInfo

--- HeatProductionOptimizer/HeatingGridAvaloniApp/Views/MainWindow.axaml.cs
+++ HeatProductionOptimizer/HeatingGridAvaloniApp/Views/MainWindow.axaml.cs
@@ -15,15 +15,18 @@ public partial class MainWindow : Window
     {
         Debug.WriteLine($"Click! UserName={userName.Text}");
 
-        if (double.TryParse(userName.Text, out double C))
+        string userNameInput = userName.Text;
+
+        if (!string.IsNullOrEmpty(userNameInput))
         {
-            var F = C * (9d / 5d) + 32;
-            password.Text = F.ToString("0.0");
-        }
-        else
-        {
-            userName.Text = "0";
-            password.Text = "0";
+            if (userNameInput == "Danfoss")
+            {
+                password.Text = userNameInput + "EmployeeAccess";
+            }
+            else
+            {
+                password.Text = "UserAccess";
+            }
         }
     }
 }
--- HeatProductionOptimizer/obj/Debug/net8.0/HeatProductionOptimizer.AssemblyInfo.cs
+++ HeatProductionOptimizer/obj/Debug/net8.0/HeatProductionOptimizer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatProductionOptimizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d3ec40e283934c3ff7a7f67f292f3957b192db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b340fbf2b3e2720c6dc78d510616a714a9d75df")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatProductionOptimizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatProductionOptimizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
