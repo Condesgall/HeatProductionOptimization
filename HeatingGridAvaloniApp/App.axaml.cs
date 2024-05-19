7f6bfddc5e4aa3bb42b076d7769a2c9a6dcd90a2
sduhubert <husyl23@student.sdu.dk>
2024-05-19 23:32:40 +0200

    Main menu test

--- HeatingGridAvaloniApp/App.axaml.cs
+++ HeatingGridAvaloniApp/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using HeatingGridAvaloniaApp.Views;
 using HeatingGridAvaloniApp.ViewModels;
 using HeatingGridAvaloniApp.Views;
 
--- HeatingGridAvaloniApp/Views/MainAppView.axaml.cs
+++ HeatingGridAvaloniApp/Views/MainAppView.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using HeatingGridAvaloniaApp.Views;
 
 namespace HeatingGridAvaloniApp.Views;
 
@@ -9,5 +11,17 @@ public partial class MainAppView : UserControl
     public MainAppView()
     {
         InitializeComponent();
+    }
+
+    public void RedirectToAM(object sender, RoutedEventArgs e)
+    {
+        var contentArea = this.FindControl<ContentControl>("ContentArea");
+        contentArea.Content = new AM_View();
+    }
+
+    public void RedirectToOptimizer(object sender, RoutedEventArgs e)
+    {
+        var contentArea = this.FindControl<ContentControl>("ContentArea");
+        contentArea.Content = new AM_View();
     }
 }
