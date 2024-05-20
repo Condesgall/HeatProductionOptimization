fab683feea443fcfa22eeceead283b3c8aa720a6
sduhubert <husyl23@student.sdu.dk>
2024-05-20 17:55:14 +0200

    cleanup

--- HeatingGridAvaloniApp/Views/MainAppView.axaml.cs
+++ HeatingGridAvaloniApp/Views/MainAppView.axaml.cs
@@ -15,19 +15,15 @@ public partial class MainAppView : UserControl
 
     public void RedirectToAM(object sender, RoutedEventArgs e)
     {
-        var buttonPanel = this.FindControl<StackPanel>("ButtonPanel");
         var openedTab = this.FindControl<ContentControl>("OpenedTab");
         
-        buttonPanel.IsVisible = false;
         openedTab.Content = new AM_View();
     }
 
     public void RedirectToOptimizer(object sender, RoutedEventArgs e)
     {
-        var buttonPanel = this.FindControl<StackPanel>("ButtonPanel");
         var openedTab = this.FindControl<ContentControl>("OpenedTab");
         
-        buttonPanel.IsVisible = false;
         openedTab.Content = new OptimizerView();
     }
 }
